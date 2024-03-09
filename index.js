6ecdd10f8f5bd1c92452ab6a840fb1c53ec8fb44
arrmdhn9 <ar.rmdhn9@gmail.com>
2024-03-10 00:34:26 +0700

    xx

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => {
-  console.log("Server running");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
