cee5047bcad61ed78ed62b30189f5b391623324d
arrmdhn9 <ar.rmdhn9@gmail.com>
2024-03-10 01:14:57 +0700

    vercel 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Get Method"));
 app.post("/", (req, res) => res.send("Post Method"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(port, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
