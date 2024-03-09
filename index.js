6440ec787d86daeee5475e39e398a9755ff2dbee
arrmdhn9 <ar.rmdhn9@gmail.com>
2024-03-10 00:39:30 +0700

    change 3

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel X"));
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Get Method"));
+app.post("/", (req, res) => res.send("Post Method"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
